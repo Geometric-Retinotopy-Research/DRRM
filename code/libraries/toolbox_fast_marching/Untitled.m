c80fa96f83eb428eeb143e681bab919222dc1ee0
ColinX <xenosxiny@icloud.com>
2022-01-06 21:46:08 -0700

    [delete] deleted files

--- code/libraries/toolbox_fast_marching/Untitled.m
+++
@@ -1,4 +0,0 @@
-% set up a C compiler
- mex -setup % uncomment this line
-% compile
-compile_mex; % uncomment this line
--- code/libraries/toolbox_fast_marching/kompilacija.m
+++
@@ -1,7 +0,0 @@
-clear all;
-fprintf('Compiling mex files ... ');
-
-% compile mex files
-mex mex/perform_front_propagation_anisotropic.cpp
-
-disp('done.');
